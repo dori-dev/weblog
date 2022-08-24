d44447d6047334ef49f13e8e9024a558c93b5659
dori-dev <mr.dori.dev@gmail.com>
2022-08-24 17:24:49 +0430

    create post models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = "draft"
+        PUBLISHED = "published"
+
+    title = models.CharField(
+        max_length=256,
+    )
+    slug = models.CharField(
+        max_length=256,
+        unique_for_date='published',
+    )
+    body = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='blog_posts',
+    )
+    created = models.DateTimeField(
+        auto_now_add=True,
+    )
+    updated = models.DateTimeField(
+        auto_now=True,
+    )
+    published = models.DateTimeField(
+        default=timezone.now,
+    )
+    status = models.CharField(
+        max_length=12,
+        choices=Status.choices,
+        default='draft',
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = (
+            '-published',
+        )
