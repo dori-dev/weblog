62da85bf0979193c08c893b065be5a4cf26131a3
dori-dev <mr.dori.dev@gmail.com>
2022-09-01 10:57:28 +0430

    customize post admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'published', 'status')
+    list_filter = ('status', 'author__username')
+    search_fields = ('title', 'body', 'author__username')
+    ordering = ('-status', 'published')
+    date_hierarchy = 'published'
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
