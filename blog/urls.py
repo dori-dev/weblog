5676678265a21aa1e6434c570f8959dab32976bb
dori-dev <mr.dori.dev@gmail.com>
2022-09-04 15:00:13 +0430

    use class based view

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('', views.PostListView.as_view(), name='post_list'),
+    path(
+        '<slug:slug>/',
+        views.PostDetailView.as_view(),
+        name='post_detail'
+    ),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,15 @@
-from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator, EmptyPage
-from django.core.handlers.wsgi import WSGIRequest
+from django.views.generic import DetailView, ListView
 from .models import Post
 
 
-def post_list(request: WSGIRequest):
-    post_objects = Post.published.all()
-    paginator = Paginator(post_objects, 12)
-    page = request.GET.get('page')
-    if page is None or not page.isdigit():
-        page = 1
-    try:
-        posts = paginator.page(page)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-    context = {
-        'posts': posts,
-    }
-    return render(request, 'blog/post/list.html', context)
+class PostListView(ListView):
+    queryset = Post.published.all()
+    paginate_by = 12
+    context_object_name = 'posts'
+    template_name = 'blog/post/list.html'
 
 
-def post_detail(request: WSGIRequest, slug: str):
-    post = get_object_or_404(Post, slug=slug, status='published')
-    context = {
-        'post': post
-    }
-    return render(request, 'blog/post/detail.html', context)
+class PostDetailView(DetailView):
+    queryset = Post.published.all()
+    context_object_name = 'post'
+    template_name = 'blog/post/detail.html'
