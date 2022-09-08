c652392ca2f13a1778bb632c5f6806054d039810
dori-dev <mr.dori.dev@gmail.com>
2022-09-08 11:18:03 +0430

    add comment form

---
+++ blog/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Comment
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = (
+            'name',
+            'email',
+            'body',
+        )
