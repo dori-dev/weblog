8251c990facffba51defc5aff3f65ad4ba4ecd55
dori-dev <mr.dori.dev@gmail.com>
2022-12-09 19:59:43 +0330

    add latest posts to base html for blog app

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -4,6 +4,16 @@ from django import template
 register = template.Library()
 
 
-@register.simple_tag
+@register.simple_tag(name='posts_count')
 def total_posts():
     return Post.published.count()
+
+
+@register.inclusion_tag(
+    'blog/post/latest_posts.html',
+    name='latest_posts')
+def show_latest_posts(count: int = 5):
+    latest_posts = Post.published.all()[:count]
+    return {
+        'latest_posts': latest_posts,
+    }
