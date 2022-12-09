1bf1ab747e84110b6fd0961ad5e5d31d116b0029
dori-dev <mr.dori.dev@gmail.com>
2022-12-09 20:14:28 +0330

    add most commented posts for show in base html

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,19 +1,39 @@
 from ..models import Post
 from django import template
+from django.db.models import Count
 
 register = template.Library()
 
 
-@register.simple_tag(name='posts_count')
+@register.simple_tag(
+    name='posts_count',
+)
 def total_posts():
     return Post.published.count()
 
 
 @register.inclusion_tag(
     'blog/post/latest_posts.html',
-    name='latest_posts')
+    name='latest_posts',
+)
 def show_latest_posts(count: int = 5):
     latest_posts = Post.published.all()[:count]
     return {
         'latest_posts': latest_posts,
+    }
+
+
+@register.inclusion_tag(
+    'blog/post/most_commented_posts.html',
+    name='most_commented_posts',
+)
+def get_most_commented_posts(count: int = 3):
+    most_commented_posts = Post.published.annotate(
+        total_comments=Count('comments')
+    ).order_by(
+        '-total_comments',
+        '-published_at',
+    )[:count]
+    return {
+        'most_commented_posts': most_commented_posts,
     }
